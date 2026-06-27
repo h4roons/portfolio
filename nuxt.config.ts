c584d1080bc2958f5bc931f506d10119fa262070
Haroon Sohail <haroonsohail323@gmail.com>
2026-06-28 02:37:55 +0500

    Add HS monogram favicon (SVG + PNG + apple-touch-icon)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,6 +72,8 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'canonical', href: 'https://haroonsohail.me' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         {
